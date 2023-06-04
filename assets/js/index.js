aa476ad38af29f2504c0f46aa2809f4def2f2d3c
Vania <vania.vnce@gmail.com>
2023-06-04 16:07:27 -0400

    proyecto listo

---
+++ assets/js/index.js
@@ -0,0 +1,17 @@
+let boton = document.querySelector('.boton')
+boton.innerHTML = onclick = " 
+let input = document.querySelector('.cantidad')
+let input1 = document.querySelector('.color')
+let parrafo = document.querySelector('.multiplicacion1')
+let parrafo1 = document.querySelector('.resultadoColor')
+let parrafo2 = document.querySelector('.cantidad2')
+let card2 = document.querySelector('.resultado')
+let multiplicacion = Number(input.value)
+let estilo = String(input1.value)
+let resultado = multiplicacion * 400000
+
+parrafo.innerHTML = 'Total: $ ' + resultado
+parrafo1.innerHTML = 'Color: ' + estilo
+parrafo2.innerHTML = 'Cantidad: ' + multiplicacion
+card2.style.border = 'solid 2px red'
+"
