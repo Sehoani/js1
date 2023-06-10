c0579a0db0fef45f8bd737aa93f2e899d668584b
Vania <vania.vnce@gmail.com>
2023-06-09 21:22:14 -0400

    correccion de errores presentados por el profesor, código en archivo externo

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,15 @@
-let boton = document.querySelector('.boton')
-boton.innerHTML = onclick = " 
+function boton(){
 let input = document.querySelector('.cantidad')
-let input1 = document.querySelector('.color')
-let parrafo = document.querySelector('.multiplicacion1')
-let parrafo1 = document.querySelector('.resultadoColor')
-let parrafo2 = document.querySelector('.cantidad2')
-let card2 = document.querySelector('.resultado')
-let multiplicacion = Number(input.value)
-let estilo = String(input1.value)
-let resultado = multiplicacion * 400000
+            let input1 = document.querySelector('.color')
+            let parrafo = document.querySelector('.multiplicacion1')
+            let parrafo1 = document.querySelector('.resultadoColor')
+            let parrafo2 = document.querySelector('.cantidad2')
+            let card2 = document.querySelector('.resultado')
+            let multiplicacion = Number(input.value)
+            let estilo = String(input1.value)
+            let resultado = multiplicacion * 400000
 
-parrafo.innerHTML = 'Total: $ ' + resultado
-parrafo1.innerHTML = 'Color: ' + estilo
-parrafo2.innerHTML = 'Cantidad: ' + multiplicacion
-card2.style.border = 'solid 2px red'
-"
+            parrafo.innerHTML = 'Total: $ ' + resultado
+            parrafo1.innerHTML = 'Color: ' + estilo
+            parrafo2.innerHTML = 'Cantidad: ' + multiplicacion
+}
